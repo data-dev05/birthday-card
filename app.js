9ec6b771f00f947180a94a91bb3899b4b416d401
data-dev05 <169947116+data-dev05@users.noreply.github.com>
2024-07-06 22:11:03 -0600

    styles

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 document.getElementById("section-1").addEventListener("click", () => {
     showCard('card-1');})
 
+document.getElementById("button-1").addEventListener("click", () => {
+    closeCard("card-1");
+})
+
+document.getElementById("section-2").addEventListener("click", () => {
+    showCard('card-2');})
+
+document.getElementById("button-2").addEventListener("click", () => {
+    closeCard("card-2");
+})
+
 function showCard(cardId) {
     document.getElementById(cardId).style.display = 'block';
     }
+
+function closeCard(cardId) {
+    document.getElementById(cardId).style.display = 'none';
+}
