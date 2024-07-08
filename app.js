96c49c6c55069ef3b4673b062dcb0eaa667a9211
data-dev05 <169947116+data-dev05@users.noreply.github.com>
2024-07-07 19:36:19 -0600

    some changes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Section 1
-document.getElementById("section-1").addEventListener("touchend", () => {
+document.getElementById("section-1").addEventListener("touchend", (event) => {
     showCard('card-1');
     event.preventDefault();})
 
@@ -8,7 +8,7 @@ document.getElementById("button-1").addEventListener("click", () => {
 })
 
 // Section 2
-document.getElementById("section-2").addEventListener("touchend", () => {
+document.getElementById("section-2").addEventListener("touchend", (event) => {
     showCard('card-2');
     event.preventDefault();})
 
@@ -17,7 +17,7 @@ document.getElementById("button-2").addEventListener("click", () => {
 })
 
 // Section 3
-document.getElementById("section-3").addEventListener("touchend", () => {
+document.getElementById("section-3").addEventListener("touchend", (event) => {
     showCard('card-3');
     event.preventDefault();})
 
@@ -35,85 +35,85 @@ document.getElementById("button-4").addEventListener("click", () => {
     closeCard("card-4");
 });
 
-// Section 5
-document.getElementById("section-5").addEventListener("touchend", (event) => {
-    showCard('card-5');
-    event.preventDefault();
-});
+// // Section 5
+// document.getElementById("section-5").addEventListener("touchend", (event) => {
+//     showCard('card-5');
+//     event.preventDefault();
+// });
 
-document.getElementById("button-5").addEventListener("click", () => {
-    closeCard("card-5");
-});
+// document.getElementById("button-5").addEventListener("click", () => {
+//     closeCard("card-5");
+// });
 
-// Section 6
-document.getElementById("section-6").addEventListener("touchend", (event) => {
-    showCard('card-6');
-    event.preventDefault();
-});
+// // Section 6
+// document.getElementById("section-6").addEventListener("touchend", (event) => {
+//     showCard('card-6');
+//     event.preventDefault();
+// });
 
-document.getElementById("button-6").addEventListener("click", () => {
-    closeCard("card-6");
-});
+// document.getElementById("button-6").addEventListener("click", () => {
+//     closeCard("card-6");
+// });
 
-// Section 7
-document.getElementById("section-7").addEventListener("touchend", (event) => {
-    showCard('card-7');
-    event.preventDefault();
-});
+// // Section 7
+// document.getElementById("section-7").addEventListener("touchend", (event) => {
+//     showCard('card-7');
+//     event.preventDefault();
+// });
 
-document.getElementById("button-7").addEventListener("click", () => {
-    closeCard("card-7");
-});
+// document.getElementById("button-7").addEventListener("click", () => {
+//     closeCard("card-7");
+// });
 
-// Section 8
-document.getElementById("section-8").addEventListener("touchend", (event) => {
-    showCard('card-8');
-    event.preventDefault();
-});
+// // Section 8
+// document.getElementById("section-8").addEventListener("touchend", (event) => {
+//     showCard('card-8');
+//     event.preventDefault();
+// });
 
-document.getElementById("button-8").addEventListener("click", () => {
-    closeCard("card-8");
-});
+// document.getElementById("button-8").addEventListener("click", () => {
+//     closeCard("card-8");
+// });
 
-// Section 9
-document.getElementById("section-9").addEventListener("touchend", (event) => {
-    showCard('card-9');
-    event.preventDefault();
-});
+// // Section 9
+// document.getElementById("section-9").addEventListener("touchend", (event) => {
+//     showCard('card-9');
+//     event.preventDefault();
+// });
 
-document.getElementById("button-9").addEventListener("click", () => {
-    closeCard("card-9");
-});
+// document.getElementById("button-9").addEventListener("click", () => {
+//     closeCard("card-9");
+// });
 
-// Section 10
-document.getElementById("section-10").addEventListener("touchend", (event) => {
-    showCard('card-10');
-    event.preventDefault();
-});
+// // Section 10
+// document.getElementById("section-10").addEventListener("touchend", (event) => {
+//     showCard('card-10');
+//     event.preventDefault();
+// });
 
-document.getElementById("button-10").addEventListener("click", () => {
-    closeCard("card-10");
-});
+// document.getElementById("button-10").addEventListener("click", () => {
+//     closeCard("card-10");
+// });
 
-// Section 11
-document.getElementById("section-11").addEventListener("touchend", (event) => {
-    showCard('card-11');
-    event.preventDefault();
-});
+// // Section 11
+// document.getElementById("section-11").addEventListener("touchend", (event) => {
+//     showCard('card-11');
+//     event.preventDefault();
+// });
 
-document.getElementById("button-11").addEventListener("click", () => {
-    closeCard("card-11");
-});
+// document.getElementById("button-11").addEventListener("click", () => {
+//     closeCard("card-11");
+// });
 
-// Section 12
-document.getElementById("section-12").addEventListener("touchend", (event) => {
-    showCard('card-12');
-    event.preventDefault();
-});
+// // Section 12
+// document.getElementById("section-12").addEventListener("touchend", (event) => {
+//     showCard('card-12');
+//     event.preventDefault();
+// });
 
-document.getElementById("button-12").addEventListener("click", () => {
-    closeCard("card-12");
-});
+// document.getElementById("button-12").addEventListener("click", () => {
+//     closeCard("card-12");
+// });
 
 
 function showCard(cardId) {
